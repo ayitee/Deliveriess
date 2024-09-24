63dae97ac977df24c1603db1c97e2a3721abb03f
ayitee <matis.gaudin@epitech.digital>
2024-09-24 15:32:38 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,7 +1,6 @@
 partieHTML:
 <h3>
-Welcome to the javascript pool!
-Have some fun and dont forget to bring a towel.
+Welcome to the javascript pool! <br> Have some fun and dont forget to bring a towel.
 </h3>
 partieJS:
 console.log("Website launched!")
